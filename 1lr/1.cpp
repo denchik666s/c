c9113434ae95817cbcd74a7d1bfa56ed670426fc
denchik666s <63151545+denchik666s@users.noreply.github.com>
2020-04-16 11:39:17 +0300

    Add files via upload

---
+++ 1lr/1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Subject"<<" "<<"Marks"<<endl<<"Mathematics"<<" "<<"90"<<endl<<"Computer"<<" "<<"77"<<endl<<"Chemistry"<<" "<<"69  solution";
+    return 0;
+}
